9360079f4a13d6518483fc8a9db071ebd43c75b5
Huan Nguyen <s3887111@rmit.edu.vn>
2021-10-12 17:43:22 +0700

    animate search bar

--- javascript/profile.js
+++ javascript/profile.js
@@ -6,5 +6,14 @@ function hide_horizontal_navbar() {
   } else {
     document.querySelector(".horizontal_navbar").style.top = "-50px";
   }
-  prevScrollpos = currentScrollPos;  
+  prevScrollpos = currentScrollPos;
+}
+
+function lighten_search_bar() {
+  var search_bar = document.querySelector(".search_bar_input");
+  search_bar.style.background="white";
+}
+function darken_search_bar() {
+  var search_bar = document.querySelector(".search_bar_input");
+  search_bar.style.background="#EBECF0";
 }
