859a79888b224f4c53142d28d66c2ebc8fcffcf1
Huan Nguyen <s3887111@rmit.edu.vn>
2021-10-13 09:49:08 +0700

    finish follow button

--- javascript/profile.js
+++ javascript/profile.js
@@ -18,8 +18,18 @@ function darken_search_bar() {
   search_bar.style.background="#EBECF0";
 }
 window.onload=function () {
+  click_count = 0;
   document.querySelector(".follow_user>button").addEventListener("click", function followed () {
-    document.querySelector(".follow_user>button").innerHTML = "Followed";
+    click_count+=1;
+    if (click_count%2==0){
+      document.querySelector(".follow_user>button").innerHTML = "Follow";
+      document.querySelector(".follow_user>button").style.background="white";
+      document.querySelector(".follow_user>button").style.color="#ff2400";
+    }else{
+      document.querySelector(".follow_user>button").innerHTML = "Followed";
+      document.querySelector(".follow_user>button").style.background="#ff2400";
+      document.querySelector(".follow_user>button").style.color="white";
+    }
   })
 
 }
