8f8d750f5ff65c0d1ae4bce4b203ea4572fbc47a
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-10-20 16:53:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,63 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const mongoose = require('mongoose');
+const userModel = require("./models");
+const bodyParserTest = require("body-parser");
+const { schema } = require('./models');
+const nodemailer = require('nodemailer');
 
+var transport = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'rmit.itstudent@gmail.com',
+      pass: 'buildingitsystem'
+    }
+})
+console.log(transport);
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+mongoose.connect('mongodb+srv://huan:buildingitsystem@cluster0.da9it.mongodb.net/user-info?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+const mongo_db = mongoose.connection;
+mongo_db.on("error", console.error.bind(console, "connection failure: "));
+mongo_db.once("open", function(){
+    console.log('hello world');
+})
 
+const userSchema = new mongoose.Schema({
+    email: {type: String, require: true},
+    password: {type: String, require: true},
+    loginAt: {type: Date, default: Date.now},
+    user: {type: String, default: "guest"}    
+})
+
+
+const chatSchema = new mongoose.Schema({
+    content: {type: String, require: true},
+    sendAt: {type: Date, default: Date.now},
+    room: {type: String, default: "Building IT System"}
+})
+const user_account = mongoose.models['user'] || mongoose.model('user', userSchema);
+const message = mongoose.models['message'] || mongoose.model('message', chatSchema);
 app.use(express.static(path.join(__dirname, 'front-end')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/login", function(req, res) {
+    let new_account = new user_account({
+        email: req.body.email_signin,
+        password: req.body.password_signin
+    });
+    new_account.save();
+    res.redirect('/profile.html');
+})
 io.on('connection', socket => {
     socket.on('message_sent', data => {
+        let new_message = new message({
+            content: data           
+        });
+        new_message.save();
         socket.broadcast.emit('user-chat', data);
     })
     socket.on('message_sender_background', data => {
@@ -18,6 +67,15 @@ io.on('connection', socket => {
     io.emit('join_chat', `user ${socket.id}  has joined the chat`)
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnect', `User ${socket.id} has left the room chat`);
+    })
+    socket.on('verify_email', receiver => {
+        console.log('socket sent');
+        transport.sendMail({
+            from: "rmit.itstudent@gmail.com",
+            to: receiver['email_address'],
+            subject: "Password Generator",
+            text: "Your password is: "+receiver['password']
+        })
     })
 })
 const PORT = process.env.PORT||3000;
