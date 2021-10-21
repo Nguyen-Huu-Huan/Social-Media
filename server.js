7cedbe6ef48a654ee9f8d4ea8f4f25246aaf6ce6
Nguyen-Huu-Huan <80439027+Nguyen-Huu-Huan@users.noreply.github.com>
2021-10-21 17:51:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,34 +24,28 @@ mongo_db.on("error", console.error.bind(console, "connection failure: "));
 mongo_db.once("open", function(){
     console.log('database connection established');
 })
-
 const userSchema = new mongoose.Schema({
-    email: {type: String, require: true},
+    email: {type: String, require: true, unique: true, dropDups: true},
     password: {type: String, require: true},
     loginAt: {type: Date, default: Date.now},
-    user: {type: String, default: "guest"}    
+    new_user_status: {type: Boolean, default: true},
+    user: {type: String, default: "guest"}, 
+    post: {type: Array, default: []}
 })
-
-
-const chatSchema = new mongoose.Schema({
+const messageSchema = new mongoose.Schema({
+    message_id: {type: Number, default: 2539},
     content: {type: String, require: true},
+    sender: {type: String, default: "guess"},
     sendAt: {type: Date, default: Date.now},
-    room: {type: String, default: "Building IT System"}
 })
-const user_account = mongoose.models['user'] || mongoose.model('user', userSchema);
-const message = mongoose.models['message'] || mongoose.model('message', chatSchema);
+
+const user_account = mongoose.models['users'] || mongoose.model('users', userSchema);
+const message = mongoose.models['message'] || mongoose.model('message', messageSchema);
 app.use(express.static(path.join(__dirname, 'front-end')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/login", function(req, res) {
-    let new_account = new user_account({
-        email: req.body.email_signin,
-        password: req.body.password_signin
-    });
-    new_account.save();
-    res.redirect('/profile.html');
-})
+
 io.on('connection', socket => {
     socket.on('message_sent', data => {
         let new_message = new message({
@@ -63,7 +57,7 @@ io.on('connection', socket => {
     socket.on('message_sender_background', data => {
         socket.emit('change_sender_chat_background', data);
     })
-    io.emit('join_chat', `user ${socket.id}  has joined the chat`)
+    
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnect', `User ${socket.id} has left the room chat`);
     })
@@ -72,9 +66,43 @@ io.on('connection', socket => {
             from: "rmit.itstudent@gmail.com",
             to: receiver['email_address'],
             subject: "Password Generator",
-            text: "Your password is: "+receiver['password']
+            text: "Your password is: "+receiver['password'],
+            html: `<h1>Want to join us?</h1><a href=\`https://building-it-project.herokuapp.com/profile.index\`>Login with new account</a>`
+        })
+        let new_account = new user_account({
+            email: receiver['email_address'],
+            password: receiver['password']
+        });
+        new_account.save();
+    })
+    socket.on('login_auth', (login_info) => {
+        user_account.find({email: login_info['email'], password: login_info['password']}, function(error, data){
+            if (data.length===0){
+                socket.emit('login_failed', '')
+            }else{
+                console.log('login success');
+                io.emit('join_chat', `user ${login_info['email'].split('@')[0]}  has joined the chat`)
+            }
         })
     })
+    // socket.on('join_chat', (user_info){
+    //     user_account.find({email: login_info['room'], password: login_info['user_id']}, function(error, data){
+    //         if (data.length===0){
+    //             socket.emit('login_failed', '')
+    //         }else{
+    //             console.log('login success');
+    //             io.emit('join_chat', `user ${login_info['email'].split('@')[0]}  has joined the chat`)
+    //         }
+    //     })
+    // })
+    // socket.on('logout', (login_info) => {
+    //     user_account.find({email: login_info['email']}, function(error, data){
+    //         console.log('login success');
+    //         io.emit('leave_chat', `user ${login_info['email'].split('@')[0]}  has left the chat`)
+    //     }
+    //     })
+    // })
+    
 })
 const PORT = process.env.PORT||3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
